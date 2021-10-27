4d38abf67125ba8d8c84db56a50603476656616b
magnuslanther <55132227+magnuslanther@users.noreply.github.com>
2021-10-27 13:21:17 +0200

    Første commit

--- App.js
+++ App.js
@@ -11,6 +11,8 @@ import {createStackNavigator} from "@react-navigation/stack";
 
 import negle from "./components/salonTypeComponents/negle";
 
+//test test
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
